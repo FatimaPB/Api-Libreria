8b7904f3572ded65e946faf7e8b622dd072736af
GABBRIELJUA <20221040@uthh.edu.mx>
2025-07-19 02:25:28 -0600

    coregoendo conexion

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -8,25 +7,18 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
-  connectionLimit: 50, // Puedes subirlo a 20 si necesitas más conexiones temporales
+  connectionLimit: 50,
   queueLimit: 0
 });
-
-// Versión con promesas (para async/await)
-const promisePool = pool.promise();
 
 // Prueba de conexión
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error('❌ Error conectando a MySQL:', err);
+    console.error('Error conectando a MySQL:', err);
   } else {
-    console.log('✅ Conexión exitosa a MySQL db Librería Cristo Rey');
+    console.log('Conexión exitosa a MySQL db Librería Cristo Rey ✅');
     connection.release();
   }
 });
 
-// Exporta ambas versiones
-module.exports = {
-  pool,         // Callbacks (tu código actual)
-  promisePool   // Promesas (para migrar a async/await)
-};
+module.exports = pool;
