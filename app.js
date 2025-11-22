e925e183da3f607f032aa49a3410f5e822b1accd
GABBRIELJUA <20221040@uthh.edu.mx>
2025-11-21 21:08:15 -0600

    se eliminó la opción 'preflightContinue' de la configuración de CORS

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ const corsOptions = {
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  preflightContinue: true
 };
 
 app.use(express.json());
